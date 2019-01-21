0ac20c03bd655fccac9fb2f70954bbede8335f12
shuu-tatsu <helloshuushuu@gmail.com>
2019-01-21 17:41:36 +0900

    init

--- corpus/medline/split_train_data.py
+++ corpus/medline/split_train_data.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 
 def make_init_list(no_abs_list, denominator):
-    #分割する最初のabs_noのリストを作成
+    #分割する最初のabs_noのリストを作成(ファイルとファイルの境界作成)
     init_list = []
     for cnt, abs_no in enumerate(no_abs_list):
         if cnt % denominator == 0:
@@ -32,7 +32,7 @@ def split(init_list, file_path):
 def split_anno(init_list):
     file_path = []
     file_path.append('./dir_id_train_anno/split_anno_0.txt')
-    file_paeh.append('./dir_id_train_anno/id_train_anno_.txt')
+    file_path.append('./dir_id_train_anno/id_train_split_anno_.txt')
     file_path.append('./dir_id_train_anno/split_anno_')
     split(init_list, file_path)
 
@@ -40,19 +40,22 @@ def split_anno(init_list):
 def split_id_train(init_list):
     file_path = []
     file_path.append('./dir_id_train/split_id_0.txt')
-    file_path.append('./dir_id_train/id_train.txt')
+    file_path.append('./dir_id_train/id_train_split.txt')
     file_path.append('./dir_id_train/split_id_')
     split(init_list, file_path)
 
 
-with open('./dir_id_train_anno/id_train_anno_.txt', 'r') as r:
+with open('./dir_id_train_anno/id_train_split_anno_.txt', 'r') as r:
     no_abs_list = [int(line.split('\t')[0]) for line in tqdm(r)]
 no_abs_set = set(no_abs_list)
 no_abs_list = list(no_abs_set)
 no_abs_list.sort()
-denominator = int(len(no_abs_list) / 100)
+denominator = int(len(no_abs_list) / 3)
 
 init_list = make_init_list(no_abs_list, denominator)
 
+#print('spliting anno')
 #split_anno(init_list)
-#split_id_train(init_list)
+
+print('spliting id')
+split_id_train(init_list)
